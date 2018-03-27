dcbc4594341a5ecdc786e54ec754779f65481d14
Antoskin <spinne92e@gmail.com>
2018-03-27 19:30:03 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+ const path = require('path');
+
+
+module.exports = {
+  entry: './scripts/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: { presets: ['es2015'] }
+                    }
+                ]
+            }
+        ]
+    }
+};
