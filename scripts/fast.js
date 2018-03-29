ea64a41cfd6e4864870006721f30e8a639cf027f
Antoskin <spinne92e@gmail.com>
2018-03-29 18:39:41 +0300

    before slider team

---
+++ scripts/fast.js
@@ -0,0 +1,38 @@
+$(document).ready( function() {
+    // console.log('!a');
+
+    $('#slider-wrap').slick({
+        dots: false,
+        infinite: false,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+} )
