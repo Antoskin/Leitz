ebe98e51705b7140c9ea1dd94fe4f21f684afb43
Antoskin <spinne92e@gmail.com>
2018-03-30 12:46:46 +0300

    before form

--- scripts/fast.js
+++ scripts/fast.js
@@ -2,12 +2,50 @@ $(document).ready( function() {
     // console.log('!a');
 
     $('#slider-wrap').slick({
-        dots: false,
+        dots: true,
         infinite: false,
         speed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+
+    //   sec slider
+    $('#team-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        // fade: true,
         responsive: [
           {
             breakpoint: 1024,
