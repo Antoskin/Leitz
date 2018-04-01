b98fee3b40b59ed151453ce4ce453c702299e08e
Antoskin <spinne92e@gmail.com>
2018-04-01 09:37:50 +0300

    before

--- scripts/fast.js
+++ scripts/fast.js
@@ -38,13 +38,17 @@ $(document).ready( function() {
 
     //   sec slider
     $('#team-slider').slick({
-        dots: false,
+        prevArrow:"</span><img class='a-left control-c prev slick-prev' src='../img/left.png'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='../img/left.png'>",
+        // centerMode: true,
+        // centerPadding: '200px',
+        dots: true,
         infinite: true,
-        speed: 1000,
-        slidesToShow: 4,
-        slidesToScroll: 3,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         // fade: true,
         responsive: [
           {
@@ -72,5 +76,32 @@ $(document).ready( function() {
           }
         ]
       });
+
+
+
+      function headSVG() {
+
+        let owl1 = $('.circle1'),
+            owl2 = $('.circle2'),
+            owl3 = $('.circle-little'),
+            l1 = $('.line1'),
+            l2 = $('.line2');
+            l3 = $('.line2little'),
+            lv1 = $('.line-vert1'),
+            lv2 = $('.line-vert2')
+
+            TweenMax.fromTo(owl1, 1, {
+              opacity:'0',
+              drawSVG:'0'
+            },
+          {
+            opacity:'1',
+            drawSVG:'100%'
+          })
+
+
+      }
+      headSVG();
+
 
 } )
