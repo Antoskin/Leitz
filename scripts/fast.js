6c7b1a297096578d8594488a31e23333c1c67f70
Antoskin <spinne92e@gmail.com>
2018-04-02 17:45:25 +0300

    spinner v2

--- scripts/fast.js
+++ scripts/fast.js
@@ -18,37 +18,60 @@ $(document).ready( function() {
   } )
 
 
+let jojo = $('.active .who-is');
+  TweenMax.from(jojo, 1 ,{ opacity:'0' }).delay(0.5)
+  
 
-  for(var i=1; i<=8; i++)
-	{
-		var html = '';
-		html += '<div class="b">';
-		html += 	'<div class="c"></div>';
-		html += '</div>';
-		$('.a').append(html);
+
+
+  // for(var i=1; i<=8; i++)
+	// {
+	// 	var html = '';
+	// 	html += '<div class="b">';
+	// 	html += 	'<div class="c"></div>';
+	// 	html += '</div>';
+	// 	$('.a').append(html);
 		
-		$('.b:nth-child(' + i + ')').css({
-			'-webkit-transform' : 'rotate(' + (i-1)*45 + 'deg)',
-				 '-moz-transform' : 'rotate(' + (i-1)*45 + 'deg)',
-					'-ms-transform' : 'rotate(' + (i-1)*45 + 'deg)',
-					 '-o-transform' : 'rotate(' + (i-1)*45 + 'deg)',
-							'transform' : 'rotate(' + (i-1)*45 + 'deg)'
-		});
-	}
+	// 	$('.b:nth-child(' + i + ')').css({
+	// 		'-webkit-transform' : 'rotate(' + (i-1)*45 + 'deg)',
+	// 			 '-moz-transform' : 'rotate(' + (i-1)*45 + 'deg)',
+	// 				'-ms-transform' : 'rotate(' + (i-1)*45 + 'deg)',
+	// 				 '-o-transform' : 'rotate(' + (i-1)*45 + 'deg)',
+	// 						'transform' : 'rotate(' + (i-1)*45 + 'deg)'
+	// 	});
+	// }
 	
 
 
 
-  var rotationSnap = 45;
-  Draggable.create("#knob", {
-      type:"rotation", //instead of "x,y" or "top,left", we can simply do "rotation" to make the object spinnable! 
-      throwProps:true, //enables kinetic-based flicking (continuation of movement, decelerating after releasing the mouse/finger)
-      snap:function(endValue) { 
-          //this function gets called when the mouse/finger is released and it plots where rotation should normally end and we can alter that value and return a new one instead. This gives us an easy way to apply custom snapping behavior with any logic we want. In this case, just make sure the end value snaps to 90-degree increments but only when the "snap" checkbox is selected.
-          return Math.round(endValue / rotationSnap) * rotationSnap;
-      }
-  });
+
   
 });
 
 
+var rotationSnap = 45;
+Draggable.create("#knob", {
+    type:"rotation", //instead of "x,y" or "top,left", we can simply do "rotation" to make the object spinnable! 
+    throwProps:true, //enables kinetic-based flicking (continuation of movement, decelerating after releasing the mouse/finger)
+    snap:function(endValue) { 
+        //this function gets called when the mouse/finger is released and it plots where rotation should normally end and we can alter that value and return a new one instead. This gives us an easy way to apply custom snapping behavior with any logic we want. In this case, just make sure the end value snaps to 90-degree increments but only when the "snap" checkbox is selected.
+        // return Math.round(endValue / rotationSnap) * rotationSnap;
+        let result = Math.round(endValue / rotationSnap) * rotationSnap;
+        // console.log(result)
+        if( result % 360 == 0 || result % 180 == 0  ) {
+          $('.b').removeClass('active');
+          $( '.b-first, .b-fifth' ).addClass('active');
+        }
+        else if( result % 90 == 0 ) {
+          $('.b').removeClass('active');
+          $('.b-third, .b-seventh').addClass('active');
+        }
+ 
+ 
+        else {
+          $('.b').removeClass('active');
+        }
+        console.log(result);
+        return result
+    }
+});
