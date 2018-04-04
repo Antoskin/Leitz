7fa08fd4122634f861233db7bd899b9eed5486fd
Antoskin <spinne92e@gmail.com>
2018-04-04 14:17:35 +0300

    dev start

--- scripts/fast.js
+++ scripts/fast.js
@@ -1,28 +1,6 @@
 $(document).ready( function() {
 
-	// anchor
-	
-
-  // for(var i=1; i<=8; i++)
-	// {
-	// 	var html = '';
-	// 	html += '<div class="b">';
-	// 	html += 	'<div class="c"></div>';
-	// 	html += '</div>';
-	// 	$('.a').append(html);
-		
-	// 	$('.b:nth-child(' + i + ')').css({
-	// 		'-webkit-transform' : 'rotate(' + (i-1)*45 + 'deg)',
-	// 			 '-moz-transform' : 'rotate(' + (i-1)*45 + 'deg)',
-	// 				'-ms-transform' : 'rotate(' + (i-1)*45 + 'deg)',
-	// 				 '-o-transform' : 'rotate(' + (i-1)*45 + 'deg)',
-	// 						'transform' : 'rotate(' + (i-1)*45 + 'deg)'
-	// 	});
-	// }
-
 	TweenMax.to('.sibl-step',1.5, { opacity:1 });
-
-
 
 })
 
