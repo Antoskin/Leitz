057fd537378606d66f3be7f7249da004d3604c3b
Antoskin <spinne92e@gmail.com>
2018-05-01 14:10:11 +0300

    feet v1

--- scripts/recidents/anima.js
+++ scripts/recidents/anima.js
@@ -19,7 +19,7 @@ export function changeSlide()  {
         tm.to(`.description `,.2,{opacity:0, x: 30})
         tm.to(`.contacts`,.2,{opacity:0})
         tm.to(`.contacts a`,.2,{x: 30})
-        tm.to(`.avatar-sl img`,.4,{scale:.8,opacity:.4})
+        tm.to(`.avatar-sl img`,.4,{opacity:.1})
         
 
        tm.to(`.name `,.9,{opacity:1, x:0}).delay(1.5)
@@ -27,7 +27,7 @@ export function changeSlide()  {
        tm.to(`.description `,.9,{opacity:1, x:0}).delay(1.9)
        tm.to(`.contacts`,.9,{opacity:1}).delay(1.6)
        tm.to(`.contacts a`,.9,{x: 0}).delay(1.6)
-       tm.to(`.avatar-sl img`,.6,{scale:1,opacity:1}).delay(1.1)
+       tm.to(`.avatar-sl img`,.6,{opacity:1}).delay(1.1)
        
         // const tl1 =  new TimelineLite();
         // tl1.to(`.contacts> a`,.2,{opacity:0})
